4e5e9045f34075db5685360565266fe649578a69
sivanWu0222 <sivan0222@vip.qq.com>
2018-07-06 10:16:55 +0800

    add questions

---
+++ 杭电刷题/2017字符串统计.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+{
+    int n,count;
+    char ch;
+    while(scanf("%d", &n) != EOF)
+    {
+        count = 0;
+        while((ch = gets()) != EOF )
+        {
+            if(ch >= '0' && ch <= '9')
+                count++;
+        }
+        printf("%d\n",count);
+    }
+
+
+    return 0;
+}
